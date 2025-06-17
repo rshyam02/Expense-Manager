c443660436661aad8558cc9ccd4c5823bc4ad260
rshyam02 <114812555+rshyam02@users.noreply.github.com>
2025-06-17 16:55:39 +0530

    Update SecurityConfig.java

--- expensetracker_backend/src/main/java/com/example/demo/SecurityConfig.java
+++ expensetracker_backend/src/main/java/com/example/demo/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() // Disable CSRF for development
+            .csrf().disable() 
             .authorizeHttpRequests((authz) -> authz
                 .anyRequest().permitAll()
             )
