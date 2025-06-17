8527cc95c27b539eb379f20663c8977ffb25fda0
rshyam02 <114812555+rshyam02@users.noreply.github.com>
2025-06-17 16:08:53 +0530

    Delete BACKEND directory

--- BACKEND/expensetracker_backend/src/main/java/com/example/demo/ExpenseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
- 
-import com.example.demo.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {}
